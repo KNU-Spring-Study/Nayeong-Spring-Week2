04882e8a62864a6e129619ce760a8494cb0d9fc0
NadudAn <dsd932@naver.com>
2023-02-06 16:22:32 +0900

    HttpSession으로 교체

--- src/main/java/com/example/springlogin/controller/UserController.java
+++ src/main/java/com/example/springlogin/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.example.springlogin.controller;
 
 import com.example.springlogin.domain.User;
+import com.example.springlogin.service.session.SessionConst;
 import com.example.springlogin.service.session.SessionManager;
 import com.example.springlogin.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -30,7 +33,7 @@ public class UserController {
      * @return
      */
     @PostMapping("/sign-up")
-    public ResponseEntity signUp(@RequestBody User user) {
+    public ResponseEntity signUp(@RequestBody User user, BindingResult bindingResult) {
         boolean flag = userService.join(user);
         if(flag) {
             log.info("회원가입 성공");
@@ -38,6 +41,7 @@ public class UserController {
         }
         else {
             log.info("회원가입 실패");
+            bindingResult.reject("signUpFail", "이미 존재하는 이메일입니다.");
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
     }
@@ -49,17 +53,28 @@ public class UserController {
      * @throws IOException
      */
     @PostMapping("/sign-in")
-    public void signIn(HttpServletResponse response,
+    public void signIn(HttpServletRequest request,
+                       HttpServletResponse response,
+                       BindingResult bindingResult,
                        @RequestBody LinkedHashMap linkedHashMap) throws IOException {
-        User loginUser = userService.logIn(linkedHashMap);
-
-        if(loginUser == null) {
+        if(bindingResult.hasErrors()) {
             log.info("홈 화면으로 리다이렉트");
             response.sendRedirect("http://localhost:8080/home");
             return;
         }
 
-        sessionManager.createSession(loginUser, response);
+        User loginUser = userService.logIn(linkedHashMap);
+
+        if(loginUser == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            log.info("홈 화면으로 리다이렉트");
+            response.sendRedirect("http://localhost:8080/home");
+            return;
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginUser);
+
         log.info("login={}", loginUser.getId());
         log.info("홈 화면으로 리다이렉트");
         response.sendRedirect("http://localhost:8080/home");
@@ -74,7 +89,11 @@ public class UserController {
     @PostMapping("/logout")
     public void logout(HttpServletRequest request,
                        HttpServletResponse response) throws IOException {
-        sessionManager.expire(request);
+
+        HttpSession session = request.getSession(false);
+        if(session != null)
+            session.invalidate();   //세션을 제거한다.
+
         log.info("logout={}", request.getSession().getId().toString());
         response.sendRedirect("http://localhost:8080/home");
     }
